3fba4d6e70c06a98c92ef5020dab7261374b3129
Felipe Mendonça <felipeguarneri@wizsolucoes.com.br>
2020-10-12 12:23:58 -0300

    ajustando

--- src/app/formdata-upload/formdata-upload.component.ts
+++ src/app/formdata-upload/formdata-upload.component.ts
@@ -19,41 +19,40 @@ export class FormdataUploadComponent {
   
   createForm() {
     this.form = this.fb.group({
-      name: ['', Validators.required],
-      avatar: null
+      titulo: ['', Validators.required],
+      imagemBinary: null
     });
   }
 
   onFileChange(event) {
     if(event.target.files.length > 0) {
       let file = event.target.files[0];
-      this.form.get('avatar').setValue(file);
+      this.form.get('imagemBinary').setValue(file);
     }
   }
 
   private prepareSave(): any {
     let input = new FormData();
-    input.append('Titulo', this.form.get('name').value);
-    input.append('imagemBinary', this.form.get('avatar').value);
+    input.append('Titulo', this.form.get('titulo').value);
+    input.append('imagemBinary', this.form.get('imagemBinary').value);
     return input;
   }
 
   onSubmit() {
     const formModel = this.prepareSave();
-    this.loading = true;    
+    this.loading = true;
     setTimeout(() => {
-      const token='Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJlYjcxODBhMi02MjBlLTRlNGMtOWY2Ni01ZTIyOTM1ZWY0ZDkiLCJlbWFpbCI6ImFkbWluQGNvbmRvbXkuY29tLmJyIiwianRpIjoiNTM4NThjNjQtYTczMi00Nzc3LWI5NDgtOTAzM2I0MWJjOWEzIiwiaWF0IjoxNjAyNTE0OTQ0LCJpc0FkbWluIjoiVHJ1ZSIsIm5iZiI6MTYwMjUxNDk0NCwiZXhwIjoxNjAyNTIyMTQ0LCJpc3MiOiJjb25kb215IiwiYXVkIjoiKiJ9.sms_uV1dz9SC4U--GK0dx_VSYYw7h9Fc7ZSoMJHgHog';
-     // const headers = { 'Authorization': token };
+      // tslint:disable-next-line: max-line-length
+      const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJlYjcxODBhMi02MjBlLTRlNGMtOWY2Ni01ZTIyOTM1ZWY0ZDkiLCJlbWFpbCI6ImFkbWluQGNvbmRvbXkuY29tLmJyIiwianRpIjoiNTM4NThjNjQtYTczMi00Nzc3LWI5NDgtOTAzM2I0MWJjOWEzIiwiaWF0IjoxNjAyNTE0OTQ0LCJpc0FkbWluIjoiVHJ1ZSIsIm5iZiI6MTYwMjUxNDk0NCwiZXhwIjoxNjAyNTIyMTQ0LCJpc3MiOiJjb25kb215IiwiYXVkIjoiKiJ9.sms_uV1dz9SC4U--GK0dx_VSYYw7h9Fc7ZSoMJHgHog';     
       const headers = new HttpHeaders()
-            .set("Authorization", token);
+            .set('Authorization', token);
       let url='https://condomy.herokuapp.com/api/Condominio/dac0cd34-9f0d-430e-8f0e-d7aa6b426b9a/ata';
 
       this.http.post<any>(url, formModel, {headers} ).subscribe(data => {
-        console.log(data);  
+        console.log(data);
         alert('done!');
         this.loading = false;
         this.status = data.sucesso;
-        
     });
 
     }, 1000);
